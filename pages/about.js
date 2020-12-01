0cffa1ca201311d0f2546823dd6988a7998c9572
tam.doan <tam.doan@codecomplete.jp>
2020-12-01 10:22:19 +0700

    call api nextjs

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,28 @@
+import Link from 'next/link';
 import Layout from '../components/MyLayout';
+function About(props) {
+  return <div>
+    <Layout>
+      <h1>Fetch Data from API</h1>
+      {
+        props.data.map((data, i) => (
+          <div>
+            <Link as={`/p/${data.id}`} href={`/post?title=${data.title}`}>
+              <a key={i}>{data.title}</a>
+            </Link>
+          </div>
+        ))
+      }
+    </Layout>
 
-class About extends React.Component {
-  static async getInitialProps(ctx) {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const json = await res.json()
-    return { stars: json.stargazers_count }
-  }
-
-  render() {
-    return <div> <Layout> 
-        Next stars: {this.props.stars}
-        </Layout> </div>
-  }
+  </div>
 }
 
-export default About
+About.getInitialProps = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  return {
+    data
+  }
+}
+export default About;
